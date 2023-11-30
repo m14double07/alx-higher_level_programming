deb350c5d5ac257e495bc05a425ae044533aa077
m14double07 <m14double07@gmail.com>
2023-11-30 04:02:56 -0800

    task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,6 +3,6 @@ if __name__ == "__main__":
     from add_0 import add
     a = 1
     b = 2
-    c = add(a,b)
-    print(f"{a} + {b} = {c}")
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+
 
